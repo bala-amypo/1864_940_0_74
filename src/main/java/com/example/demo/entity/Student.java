7ab90643c179d04a00d7b183c0275ef5a9517a5d
bala-amypo <bala@amypo.in>
2025-12-18 09:26:07 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,6 +1,5 @@
 package com.example.demo.entity;
 
-import java.time.LocalDate;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -20,7 +19,7 @@ public class Student {
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     
 
-    private Long id;
+    private int id;
     @NotBlank(message="Name filed cannot be empty")
     @Size(min=3,max=20,message="The user name must be min of 3 and max of 20 character")
     private String name;
@@ -35,12 +34,10 @@ public class Student {
     //@Max
     //@Pattern(reg)
     
-    private LocalDate dob;
-    private float cgpa;
-    public Long getId() {
+    public int getId() {
         return id;
     }
-    public void setId(Long id) {
+    public void setId(int id) {
         this.id = id;
     }
     public String getName() {
@@ -55,24 +52,11 @@ public class Student {
     public void setEmail(String email) {
         this.email = email;
     }
-    public LocalDate getDob() {
-        return dob;
-    }
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
-    public float getCgpa() {
-        return cgpa;
-    }
-    public void setCgpa(float cgpa) {
-        this.cgpa = cgpa;
-    }
-    public Student(Long id, String name, String email, LocalDate dob, float cgpa) {
+    public Student(int id, String name, String email) {
         this.id = id;
         this.name = name;
         this.email = email;
-        this.dob = dob;
-        this.cgpa = cgpa;
+
     }
     public Student() {
     }
